49dd1e12e682566532e7cec08c9a165697b583cb
Anastasiia Baidina <nastiabaydina@gmail.com>
2023-10-04 11:38:50 +0300

    commit3

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,3 +1,5 @@
+import * as bodyScrollLock from 'body-scroll-lock';
+
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
